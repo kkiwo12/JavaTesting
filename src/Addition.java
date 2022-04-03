f747ac81fe3348064483c0ff034580e39347ae66
kkiwo <rebosuravan@gmail.com>
2022-04-04 00:31:54 +0800

    upload all

--- src/Addition.java
+++ src/Addition.java
@@ -3,4 +3,9 @@ public class Addition implements IOperation{
     public double execute(double firstNum, double secondNum) {
         return firstNum + secondNum;
     }
+
+    @Override
+    public int number(double num22, double num23) {
+        return 0;
+    }
 }
